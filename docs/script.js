b72aee08d8d3f5ab5afdb230edbc160db24c252f
Dylan Nirvana <uxdev@News-MacBook-Pro.local>
2018-09-07 03:10:29 -0400

    corrects spacing

--- docs/script.js
+++ docs/script.js
@@ -5,17 +5,20 @@
 //   gutter: 10
 // });
 
+// change grid to gridp to avoid clash with Bootstrap global ?
+// grid-item however is safe
+
 // initialize Packery
-var $grid = $('.grid').packery({
+var $gridp = $('.gridp').packery({
   itemSelector: '.grid-item',
-  gutter: 10,
+  // gutter: 10,
   // columnWidth helps with drop positioning
-  columnWidth: 260
+  // columnWidth: 300
 });
 
 // make all grid-items draggable
-$grid.find('.grid-item').each( function( i, gridItem ) {
+$gridp.find('.grid-item').each( function( i, gridItem ) {
   var draggie = new Draggabilly( gridItem );
   // bind drag events to Packery
-  $grid.packery( 'bindDraggabillyEvents', draggie );
+  $gridp.packery( 'bindDraggabillyEvents', draggie );
 });
