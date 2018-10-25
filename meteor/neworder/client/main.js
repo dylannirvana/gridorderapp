27bd38ace7193449424e9cd225561ef90ce53b9a
Dylan Nirvana <dylan@dylannirvana.com>
2018-10-25 09:05:09 -0400

    console logs event in handler

--- meteor/neworder/client/main.js
+++ meteor/neworder/client/main.js
@@ -28,11 +28,12 @@ Template.upload.helpers({
 
 Template.upload.events({
   'change [name="uploadCSV"]' ( event, template ) {
-    console.log(event);
+    console.log("This is the event from the handler " + event);
     // We'll handle the conversion and upload here.
     Papa.parse( event.target.files[0], {
       header: true,
       complete( results, file ) {
+        console.log(" This is the results object from PP " + results)
         // Handle the upload here.
       }
     });
