d78b27c36a6042ffab078614d716e890d2048f92
Dylan Nirvana <dylan@dylannirvana.com>
2018-10-25 09:21:26 -0400

    adds parseUpload method

--- meteor/neworder/client/main.js
+++ meteor/neworder/client/main.js
@@ -30,11 +30,19 @@ Template.upload.events({
   'change [name="uploadCSV"]' ( event, template ) {
     console.log("This is the event from the handler " + event);
     // We'll handle the conversion and upload here.
-    Papa.parse( event.target.files[0], {
+      Papa.parse( event.target.files[0], {
       header: true,
       complete( results, file ) {
         console.log(" This is the results object from PP " + results)
+        console.log("event.target.files[0] here " + event.target.files[0] )
         // Handle the upload here.
+        Meteor.call( 'parseUpload', results.data, ( error, response ) => {
+         if ( error ) {
+           Bert.alert( error.reason, 'warning' );
+         } else {
+           // Handle success here.
+         }
+       });
       }
     });
   }
