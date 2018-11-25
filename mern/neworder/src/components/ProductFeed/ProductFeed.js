14eb75126b8f3af482d32f18fe81622c1835f533
Dylan Nirvana <dylan@dylannirvana.com>
2018-11-25 11:01:00 -0500

    TODO: render to grid, iterate, have access to packery, draggabilly

--- mern/neworder/src/components/ProductFeed/ProductFeed.js
+++ mern/neworder/src/components/ProductFeed/ProductFeed.js
@@ -33,3 +33,30 @@ function uploadHandler(event) {
 }
 
 export default ProductFeed;
+
+// // INITIALIZE PACKERY
+// var $grid = $('.grid').packery({
+//   itemSelector: '.grid-item',
+//   // gutter: 10,
+//   // columnWidth helps with drop positioning
+//   columnWidth: 240
+// });
+
+// // INITIALIZE DRAGGABILLY
+// $grid.find('.grid-item').each( function( i, gridItem ) {
+//   var draggie = new Draggabilly( gridItem );
+//   // bind drag events to Packery
+//   $grid.packery( 'bindDraggabillyEvents', draggie );
+// });
+
+// // // INITIALIZE ISOTOPE
+// // var $grid = $('.grid').isotope({
+// // $grid.isotope({
+// //   getSortData: {
+// //     name: '.name', // text from querySelector
+// //     category2: '.category2',
+// //     function2: '.function2',
+// //     family: '.family'
+// //     // category: '[data-category]' // value of attribute
+// //   }
+// // });
