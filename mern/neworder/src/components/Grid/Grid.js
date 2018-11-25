d439f4d1ff580c3d1038cb869b546e5323361ad8
Dylan Nirvana <dylan@dylannirvana.com>
2018-11-25 10:45:51 -0500

    TODO: render to grid, iterate, have access to packery, draggabilly

--- mern/neworder/src/components/Grid/Grid.js
+++ mern/neworder/src/components/Grid/Grid.js
@@ -1,3 +1,22 @@
+import React, { Component } from 'react';
+
+// TODO: Iterate over data in Bootstrap .row .col-md 
+// TODO: Make sure that Packery, Draggabilly have access .grid .grid-item 
+// And Isotope can access taxonomy
+
+class Grid extends Component {
+    render() {
+        const { itemList } = this.props;
+
+        return (
+            <div className="row grid">
+
+            </div>
+        );
+    }
+}
+
+
 // import React, { Component } from 'react';
 // import ProductFeed from '../ProductFeed';
 // import './Grid.css';
@@ -8,7 +27,7 @@
 //             <div>
 //                 <div className="row grid">
 //                     <div className="col-md-4 grid-item">
-//                         {items.itemcode}
+//                         {items.itemcode} // out of scope. use import export objects
 //                     </div>
 //                 </div>
 //             </div>
--- mern/neworder/src/components/ProductFeed/ProductFeed.js
+++ mern/neworder/src/components/ProductFeed/ProductFeed.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Input, InputGroup } from 'reactstrap';
 import Papa from 'papaparse';
 
+// Input that takes in full CSV product feed 
 const ProductFeed = () => {
   return (
     <div>
@@ -12,28 +13,21 @@ const ProductFeed = () => {
   )
 }
 
+// Handler Papa.parse converts CSV to JSON
 function uploadHandler(event) {
-  console.log('The event is registering ' + event.target)
+  console.log('The event is registering ' + event.target) // Easy squeezy
 
   const inventory = event.target.files[0];
-  console.log(inventory)
+  console.log(inventory) // The array object
 
   Papa.parse(inventory, {
     header: true,
-    complete: function(results) {
+    complete: function(results) { // Object with data. Err, meta to be handled later
       console.log(results)
 
-      let items = results.data;
+      let items = results.data; // Data to be passed to Grid, iterated over, and used by Packery, Draggabilly, and Isotope
       console.log(items)
-      
-      // return (
-      //   <div>
-      //     {items.map(function(item, index){
-      //       return <li key={index}>{item}</li>
-      //     })}
-      //   </div>
-      // )
-      
+            
     }
   })
 }
