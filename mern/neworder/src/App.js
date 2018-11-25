ae0e60cfdbec86257d3ebb34a33679366c71639f
Dylan Nirvana <dylan@dylannirvana.com>
2018-11-25 14:29:53 -0500

    just look at index.js too see everything in one place

--- mern/neworder/src/App.js
+++ mern/neworder/src/App.js
@@ -1,81 +1,94 @@
-import React, { Component } from 'react';
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Container,
-    Row,
-    Col,
-    Jumbotron,
-} from 'reactstrap';
-import ProductFeed from './components/ProductFeed';
-import Grid from './components/Grid';
+// import React, { Component } from 'react';
+// import {
+//     Collapse,
+//     Navbar,
+//     NavbarToggler,
+//     NavbarBrand,
+//     Nav,
+//     NavItem,
+//     NavLink,
+//     Container,
+//     Row,
+//     Col,
+//     Jumbotron,
+// } from 'reactstrap';
+// import ProductFeed from './components/ProductFeed';
+// import Grid from './components/Grid';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
 
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
-    }
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
-    render() {
-        return (
-            <div>
-                <Navbar color="dark" dark expand="lg">
-                    <NavbarBrand href="/">Grid Order Tool</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/components/">Hello Colleen!</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://github.com/dylannirvana/gridorder">Github</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-                <Jumbotron>
-                    <Container>
-                        <Row>
-                            <Col>
-                                <h1>Welcome to GO App</h1>                    
-                                  <ProductFeed />                              
-                            </Col>
-                        </Row>
-                    </Container>
-                </Jumbotron>
-            </div>
-        );
-    }
+//         this.toggle = this.toggle.bind(this);
+//         this.state = {
+//             isOpen: false
+//         };
+//     }
+//     toggle() {
+//         this.setState({
+//             isOpen: !this.state.isOpen
+//         });
+//     }
 
-    render() {
-        return (
-            <div>
-                <Jumbotron>
-                    <Container>
-                        <Row>
-                            <Col>
-                                <h1>Welcome to GO App</h1>                    
-                                  <Grid />                              
-                            </Col>
-                        </Row>
-                    </Container>
-                </Jumbotron>
-            </div>
-        );
-    }
-}
 
-export default App;
+//     // Input that takes in full CSV product feed 
+// const ProductFeed = () => {
+//     return (
+//       <div>
+//       <InputGroup>
+//       <Input type="file" name="inputCSV" onChange={uploadHandler}/>
+//       </InputGroup>
+  
+//       </div>
+//     )
+//   }
+
+  
+    
+// // Handler Papa.parse converts CSV to JSON
+// function uploadHandler(event) {
+  
+//     const inventory = event.target.files[0];
+  
+//     Papa.parse(inventory, {
+//       header: true,
+//       complete: function(results) {     
+//         const Items = results.data;
+//       }
+//     })
+//   }
+  
+    
+//     render() {
+//         return (
+//             <div>
+//                 <Navbar color="dark" dark expand="lg">
+//                     <NavbarBrand href="/">Grid Order Tool</NavbarBrand>
+//                     <NavbarToggler onClick={this.toggle} />
+//                     <Collapse isOpen={this.state.isOpen} navbar>
+//                         <Nav className="ml-auto" navbar>
+//                             <NavItem>
+//                                 <NavLink href="/components/">Hello Colleen!</NavLink>
+//                             </NavItem>
+//                             <NavItem>
+//                                 <NavLink href="https://github.com/dylannirvana/gridorder">Github</NavLink>
+//                             </NavItem>
+//                         </Nav>
+//                     </Collapse>
+//                 </Navbar>
+//                 <Jumbotron>
+//                     <Container>
+//                         <Row>
+//                             <Col>
+//                                 <h1>Welcome to GO App</h1>                    
+//                                   <ProductFeed />                              
+//                             </Col>
+//                         </Row>
+//                     </Container>
+//                 </Jumbotron>
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
--- mern/neworder/src/components/Grid/Grid.js
+++ mern/neworder/src/components/Grid/Grid.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import { Items } from '../ProductFeed';
-
-// TODO: Iterate over data in Bootstrap .row .col-md 
-// TODO: Make sure that Packery, Draggabilly have access .grid .grid-item 
-// And Isotope can access taxonomy
-
-class Grid extends Component {
-    render() {
-
-        return (
-            <div className="row grid">
-                <Items />
-            </div>
-        );
-    }
-}
-
-
 // import React, { Component } from 'react';
-// import ProductFeed from '../ProductFeed';
-// import './Grid.css';
+// import { Items } from '../ProductFeed';
+
+// // TODO: Iterate over data in Bootstrap .row .col-md 
+// // TODO: Make sure that Packery, Draggabilly have access .grid .grid-item 
+// // And Isotope can access taxonomy
 
 // class Grid extends Component {
 //     render() {
+
 //         return (
-//             <div>
-//                 <div className="row grid">
-//                     <div className="col-md-4 grid-item">
-//                         {items.itemcode} // out of scope. use import export objects
-//                     </div>
-//                 </div>
+//             <div className="row grid">
+//                 <Items />
 //             </div>
-//         )
+//         );
 //     }
 // }
-// console.log('Hey there ' + items.itemcode)
 
 
-export default Grid;
+// // import React, { Component } from 'react';
+// // import ProductFeed from '../ProductFeed';
+// // import './Grid.css';
+
+// // class Grid extends Component {
+// //     render() {
+// //         return (
+// //             <div>
+// //                 <div className="row grid">
+// //                     <div className="col-md-4 grid-item">
+// //                         {items.itemcode} // out of scope. use import export objects
+// //                     </div>
+// //                 </div>
+// //             </div>
+// //         )
+// //     }
+// // }
+// // console.log('Hey there ' + items.itemcode)
+
+
+// export default Grid;
 
 
 
