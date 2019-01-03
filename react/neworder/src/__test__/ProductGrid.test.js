93c33cb9fbd20dc80b685cbca02a7720750ea0f2
Faraaz Malak <faraazmalak@gmail.com>
2019-01-03 22:13:00 +0530

    Updated Unit Tests

--- react/neworder/src/__test__/ProductGrid.test.js
+++ react/neworder/src/__test__/ProductGrid.test.js
@@ -1,13 +1,8 @@
 import React, {Component} from 'react';
 import renderer from 'react-test-renderer';
-import {mount, shallow} from 'enzyme';
 import Enzyme from 'enzyme';
-import toJson from 'enzyme-to-json';
 import {createSerializer} from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
-
-import ProductGrid from '../components/ProductGrid/ProductGrid';
-import Product from '../components/ProductGrid/Product';
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() })
@@ -15,79 +10,129 @@ expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
 
 const DATA = JSON.parse('[{"sku":"81134","name":"Studio Swing Arm Floor Lamp","category":"Floor > Task","function":"Functional","relatives":"VC CLASSIC","designer":"Studio VC","image":"https://www.circalighting.com/media/catalog/product/8/1/81134hab_5.png","gridorder":"1215.0000","neworder":"","__parsed_extra":[""]},{"sku":"82034","name":"Studio Swing Arm Wall Light","category":"Wall > Task","function":"Functional","relatives":"VC CLASSIC","designer":"Studio VC","image":"https://www.circalighting.com/media/catalog/product/8/2/82034hab_1.png","gridorder":"4742.0000","neworder":"","__parsed_extra":[""]},{"sku":"91025","name":"Studio Adjustable Floor Lamp","category":"Floor > Task","function":"Functional","relatives":"VC CLASSIC","designer":"Studio VC","image":"https://www.circalighting.com/media/catalog/product/9/1/91025hab_5.png","gridorder":"1211.0000","neworder":"","__parsed_extra":[""]},{"sku":"92000D","name":"Classic Swing Arm Wall Lamp","category":"Wall > Task","function":"Functional","relatives":"VC CLASSIC","designer":"Studio VC","image":"https://www.circalighting.com/media/catalog/product/9/2/92000dhabl_1.png","gridorder":"4536.0000","neworder":"","__parsed_extra":[""]},{"sku":"92025","name":"Studio Swing Arm Wall Light","category":"Wall > Task","function":"Functional","relatives":"VC CLASSIC","designer":"Studio VC","image":"https://www.circalighting.com/media/catalog/product/9/2/92025hab_1.png","gridorder":"4738.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2000","name":"Dean Single Arm Sconce","category":"Wall > Decorative","function":"Decorative","relatives":"Dean","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2000pnnp_5.png","gridorder":"4159.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2001","name":"Dean Library Sconce","category":"Wall > Decorative","function":"Decorative","relatives":"Dean","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2001pnnp_8.png","gridorder":"4161.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2002","name":"Abbot Single Arm Sconce","category":"Wall > Decorative","family": "test-family", "function":"Decorative","relatives":"Abbot","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2002pnnp_8.png","gridorder":"4144.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2003","name":"Abbot Library Sconce","category":"Wall > Decorative","function":"Decorative","relatives":"Abbot","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2003gmnp_5.png","gridorder":"4146.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2004","name":"Bing Single Arm Sconce","category":"Wall > Decorative","function":"Decorative","relatives":"Bing","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2004pnnp_5.png","gridorder":"4137.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2010","name":"Ginger Single Arm Sconce","category":"Wall > Decorative","function":"Decorative","relatives":"Ginger","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2010pnnp_5.png","gridorder":"4139.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2012","name":"Gene Swing Arm","category":"Wall > Task","function":"Functional","relatives":"Gene","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2012nbs_1.png","gridorder":"4525.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2013","name":"Gene Library Sconce","category":"Wall > Decorative", "family": "test-family", "function":"Decorative","relatives":"Gene","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2013nbnp_5.png","gridorder":"4150.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2700","name":"Dean 9\\" Picture Light","category":"Wall > Picture","function":"Picture","relatives":"Dean2","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2700nb_1.png","gridorder":"5324.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2701","name":"Dean 12\\" Picture Light","category":"Wall > Picture","function":"Picture","relatives":"Dean2","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2701nb_1.png","gridorder":"5328.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2702","name":"Dean 18\\" Picture Light","category":"Wall > Picture","function":"Picture","relatives":"Dean2","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2702nb_1.png","gridorder":"5332.0000","neworder":"","__parsed_extra":[""]},{"sku":"AH2703","name":"Dean 24\\" Picture Light","category":"Wall > Picture","function":"Picture","relatives":"Dean2","designer":"Alexa Hampton","image":"https://www.circalighting.com/media/catalog/product/a/h/ah2703nb_1.png","gridorder":"5336.0000","neworder":"","__parsed_extra":[""]}]');
 
+//Initialize App instance
+const APP_INSTANCE = renderer.create(
+    <App  />
+).getInstance();
 
-describe('<ProductGrid/>',()=>{
-    const APP_INSTANCE = renderer.create(
-        <App  />
-    ).getInstance();
-//console.log(APP_INSTANCE)
-    APP_INSTANCE.container.setState({
-        feed: Array.from(DATA),
-        filteredProducts: DATA,
-        packeryRefresh: false
-    });
-    const FILTER_FACTORY = APP_INSTANCE.container.getFilterFactory();
-
-    it('Verify Snapshot', () => {
+//Set the initial App state
+APP_INSTANCE.container.setState({
+    feed: Array.from(DATA),
+    filteredProducts: DATA,
+    packeryRefresh: false
+});
 
 
+const FILTER_FACTORY = APP_INSTANCE.container.getFilterFactory(); //Filter Factory
+const FEED = FILTER_FACTORY.getFeed();
+const EXPECTED_PRODUCT_COUNT = {category: 15, function: 7, family: 7}; //Expected product count for each filter
 
 
-        const PRODUCT_GRID_COMPONENT = renderer.create(
-            <ProductGrid container={APP_INSTANCE.container} />
-        );
 
-        // const PRODUCT_GRID = PRODUCT_GRID_COMPONENT.getInstance();
 
-        expect(PRODUCT_GRID_COMPONENT).toMatchSnapshot()
-    });
 
-    it('Verify if selecting Category -> Floor returns 15 products'  ,()=>{
-       // console.log(FILTER_FACTORY)
-        const FILTERED_PRODUCTS = FILTER_FACTORY.toggleFilterOption('category','wall');
+/*
+* Toggles filter option
+ */
+function toggleFilterOption(filterName, selectedOption, deselect = false){
 
-        //EXPECT: Correct product count is returned returned
-        expect(FILTERED_PRODUCTS).toHaveLength(15);
+    const FILTERED_PRODUCTS = FILTER_FACTORY.toggleFilterOption(filterName,selectedOption);
 
+    if(!deselect){
         //EXPECT: Category attribute of each product to contain the keyword - Wall
-        FILTERED_PRODUCTS.forEach((product) => expect(product.category).toContain('Wall'))
+        FILTERED_PRODUCTS.forEach((product) => {
+            if( product[filterName] !== undefined){
+                expect(product[filterName].toLowerCase()).toContain(selectedOption);
+            }
 
-        //EXPECT: Result is same as the saved snapshot
-        expect(FILTERED_PRODUCTS).toMatchSnapshot();
+        })
+    }
+    return FILTERED_PRODUCTS;
+}
 
-    })
 
-    it('Verify if selecting Function -> Decorative returns 7 products'  ,()=>{
 
-        //Select the option
-        const FILTERED_PRODUCTS = FILTER_FACTORY.toggleFilterOption('function','decorative');
 
-        //EXPECT: Correct product count is returned
-        expect(FILTERED_PRODUCTS).toHaveLength(7);
+describe('Initial check of <ProductGrid /> Component',()=>{
 
-        //EXPECT: Function attribute of each product to contain the keyword - Decorative
-        FILTERED_PRODUCTS.forEach((product) => expect(product.function).toContain('Decorative'))
 
-        //EXPECT: Result is same as the saved snapshot
-        expect(FILTERED_PRODUCTS).toMatchSnapshot();
+    it(`Product grid populated with ${DATA.length} products`, () => {
+        expect(FEED).toHaveLength(DATA.length);
+    });
 
-    })
 
-    it('Verify if sorting by family "test-family" sorts correctly'  ,()=>{
+})
+
+
+
+
+
+describe('Verifying Filtering Logic',() => {
+    for(let i =0; i < 3; i++){
+
+        const SELECT = i%2 == 0 ? true : false;
+
+
+        it(`${SELECT ? 'SELECTING' : 'DESELECTING'} Category -> Wall; Expected ${i%2 == 0 ? EXPECTED_PRODUCT_COUNT.category : DATA.length} products`  ,()=>{
+
+            const FILTERED_PRODUCTS = toggleFilterOption('category','wall', !SELECT);
+
+            //EXPECT: Correct product count is returned returned
+            expect(FILTERED_PRODUCTS).toHaveLength(SELECT ? EXPECTED_PRODUCT_COUNT.category : DATA.length);
+
+
+
+        })
+    }
+
+    for(let i =0; i < 3; i++){
+
+        const SELECT = i%2 == 0 ? true : false;
+
+
+
+        it(`${SELECT ? 'SELECTING' : 'DESELECTING'} Function -> Decorative; Expected ${i%2 == 0 ? EXPECTED_PRODUCT_COUNT.function : EXPECTED_PRODUCT_COUNT.category} products`  ,()=>{
+
+
+            const FILTERED_PRODUCTS = toggleFilterOption('function','decorative', !SELECT);
+
+            //EXPECT: Correct product count is returned returned
+            expect(FILTERED_PRODUCTS).toHaveLength(SELECT ? EXPECTED_PRODUCT_COUNT.function : EXPECTED_PRODUCT_COUNT.category);
+
+
+        })
+
+
+    }
+})
+
+
+
+
+
+describe('Verifying Sorting Logic -> Sorting 2 products from the data set, with family == "test-family"',()=>{
+
+
+    //Test Data has two products with family == "test-family"
+    it('Verifying the two "test-family" products are not the first two products of the grid'  ,()=>{
 
         //Select the sort option
-        const FILTERED_PRODUCTS = FILTER_FACTORY.toggleFilterOption('family','test-family');
+        const FILTERED_PRODUCTS = toggleFilterOption('family','test-family');
 
         //FILTER_FACTORY should be ready to sort
         const SHOULD_SORT = FILTER_FACTORY.shouldSort('family')
         expect(SHOULD_SORT).toEqual(true);
 
-        //Test Data has two products with family == "test-family"
 
         //Before sort, first two products should not be having Family attribute's value "test-family"
         !expect(FILTERED_PRODUCTS[0].family).toEqual('test-family');
         !expect(FILTERED_PRODUCTS[1].family).toEqual('test-family');
 
+
+
+    })
+
+    it('After sorting, verifying the products with family == "test-family" are the first two products of the grid',()=>{
         //Do the sorting
         const SORTED_PRODUCTS = FILTER_FACTORY.sortProducts({filterName: "family", selectedOption:"test-family"});
 
@@ -98,7 +143,19 @@ describe('<ProductGrid/>',()=>{
         expect(SORTED_PRODUCTS[0].family).toEqual('test-family');
         expect(SORTED_PRODUCTS[1].family).toEqual('test-family');
 
-        //Create snapshot
-        expect(SORTED_PRODUCTS).toMatchSnapshot();
+
+    })
+
+
+})
+
+
+
+
+
+//Save the new order grid to a snapshot
+describe('Saving New Grid Order to snapshot',() => {
+    it('Snapshot created',()=>{
+        expect(FILTER_FACTORY.getFilteredProducts()).toMatchSnapshot();
     })
 })
