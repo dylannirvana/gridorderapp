904c5aa92402be1ac9347337fb734de5fd015d62
Dylan Nirvana <dylan@dylannirvana.com>
2019-01-04 14:07:00 -0500

    static components

--- react/neworder/src/App.js
+++ react/neworder/src/App.js
@@ -1,13 +1,73 @@
 import React, { Component } from 'react'
-import { Input, InputGroup, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Container, Row, Col, Jumbotron } from 'reactstrap';
+import { 
+    Card, 
+    CardImg, 
+    CardText, 
+    CardBody,
+    CardTitle, 
+    CardSubtitle, 
+    Button, 
+    Input, 
+    InputGroup, 
+    Collapse, 
+    Navbar, 
+    NavbarToggler, 
+    NavbarBrand, 
+    Nav, 
+    NavItem, 
+    NavLink, 
+    Container, 
+    Row, 
+    Col, 
+    Jumbotron 
+    } from 'reactstrap';
 import Papa from 'papaparse'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 
-
-const ProductUpload = () => {
-    const uploadHandler = (e) => {
+const ItemCard = (props) => {
+    return (
+        <Row>
+            <Col>
+                <Card>
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle> {props.name} </CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Button</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col>
+                <Card>
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Button</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col>
+                <Card>
+                    <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Button</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+    );
+  };
+    
+class App extends Component {
+    uploadHandler = (e) => {
         e.preventDefault();
         const inventory = e.target.files[0]
         Papa.parse(inventory, {
@@ -15,20 +75,11 @@ const ProductUpload = () => {
           complete: function(results) {
             const items = results.data;
             console.log(items)
+            
           } 
         })
       }
-    
-    return (
-        <div>
-          <InputGroup>
-            <Input type="file" name="inputCSV" onChange={uploadHandler} />
-          </InputGroup>
-        </div>
-      )
-}
 
-class App extends Component {
     render() {
         return (
             <div>
@@ -51,13 +102,19 @@ class App extends Component {
                         <Row>
                             <Col>
                                 <h1>Import</h1>                    
-                                <ProductUpload />                          
+                                <InputGroup>
+                                    <Input type="file" name="inputCSV" onChange={this.uploadHandler} />
+                                </InputGroup>                          
                             </Col>
                         </Row>
                         <Row>
                             <Col>
                                 <h1>The Grid</h1>  
-                                {/* <ProductGrid /> */}
+                                <ItemCard
+                                    name={props.name} 
+                                    
+
+                                />
                             </Col>
                         </Row>
                     </Container>
